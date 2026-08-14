3b6f569347bd236840575d81e5adb786aebee4a4
lim <milkystar516@gmail.com>
2026-08-14 23:59:40 +0900

    chores:스키마 양식 통일

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,20 +1,20 @@
 CREATE TABLE users (
-    id                  bigserial primary key,
-    username            varchar(50) unique not null,
+    id                  bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username            varchar(50) UNIQUE NOT NULL,
     nickname            varchar(50),
     password_hash       text
 );
 
 CREATE TABLE todos (
-    id                  bigserial primary key,
-    owner_id            bigint references users(id) on delete cascade,
-    title               varchar(255) not null,
-    created_at          timestamptz not null,
+    id                  bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    owner_id            bigint REFERENCES users(id) ON DELETE CASCADE,
+    title               varchar(255) NOT NULL,
+    created_at          timestamptz NOT NULL,
     completed_at        timestamptz
 );
 
 CREATE TABLE sessions (
-    token        text primary key,
-    user_id uuid not null references user_id(id) on delete cascade,
-    expires_at   timestamptz not null
+    token        text PRIMARY KEY,
+    user_id      bigint NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    expires_at   timestamptz NOT NULL
 );
