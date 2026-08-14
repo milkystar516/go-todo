85c812c1b922d49d4fcc24b171afb22f410dda5f
lim <milkystar516@gmail.com>
2026-08-15 00:24:21 +0900

    feature:todo와 헨들러 구현

--- backend/internal/todo/todo.go
+++ backend/internal/todo/todo.go
@@ -1,1 +1,28 @@
 package todo
+
+import (
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Handler struct {
+	db *pgxpool.Pool
+}
+
+type Todo struct {
+	ID        int64     `json:"id"`
+	OwnerID   int64     `json:"owner_id"`
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type TodoCreateRequest struct {
+	Title string `json:"title"`
+}
+
+func NewHandler(db *pgxpool.Pool) *Handler {
+	return &Handler{db: db}
+}
