bf96442894c108eb3c79717cf21109fed179eddd
lim <milkystar516@gmail.com>
2026-08-15 00:34:02 +0900

    feature:todo 등록 추가

--- backend/internal/todo/todo.go
+++ backend/internal/todo/todo.go
@@ -1,9 +1,12 @@
 package todo
 
 import (
+	"encoding/json"
+	"net/http"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/milkystar516/go-todo/backend/internal/auth"
 )
 
 type Handler struct {
@@ -25,4 +28,32 @@ type TodoCreateRequest struct {
 
 func NewHandler(db *pgxpool.Pool) *Handler {
 	return &Handler{db: db}
+}
+
+func (h *Handler) RegisterRoutes(mux *http.ServeMux, requireAuth func(http.Handler) http.Handler) {
+	mux.Handle("POST /todos", requireAuth(http.HandlerFunc(h.createTodo)))
+}
+
+func (h *Handler) createTodo(w http.ResponseWriter, r *http.Request) {
+	var req TodoCreateRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	userID := auth.UserID(r.Context())
+
+	_, err := h.db.Exec(
+		r.Context(),
+		"INSERT INTO todos (owner_id, title) VALUES ($1, $2)",
+		userID,
+		req.Title,
+	)
+	if err != nil {
+		http.Error(w, "server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
