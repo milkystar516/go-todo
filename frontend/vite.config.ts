364fd9b0ca6e72f2ec23eb10a360970f729d7895
lim <milkystar516@gmail.com>
2026-08-20 19:36:36 +0900

    chores:프론트측의 설정 추가

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, "..", "");
+
+  return {
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        "/api": {
+          target: env.API_PROXY_TARGET,
+        },
+      },
+    },
+  };
+});
