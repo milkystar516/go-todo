ea2f4803b2489b6a0479ce84b233ce6ce6a5f423
lim <milkystar516@gmail.com>
2026-08-24 16:37:19 +0900

    chores:스키마 수정

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -15,7 +15,9 @@ CREATE TABLE sessions (
 CREATE TABLE todo_rule (
     id                  bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     rule_name           varchar(50) NOT NULL,
-    fields              jsonb NOT NULL
+    content_schema      jsonb NOT NULL,
+    ui_schema           jsonb NOT NULL DEFAULT '{}'::jsonb,
+    list_columns        jsonb NOT NULL DEFAULT '{}'::jsonb
 );
 
 CREATE TABLE todos (
