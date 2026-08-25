cd31b6714c93fb5ff37680de2eba3ea36b916141
lim <milkystar516@gmail.com>
2026-08-25 18:50:12 +0900

    bugfix:누락된 applayout의 outlet wrapper 스타일 복구

--- frontend/src/app/layouts/AppLayout.tsx
+++ frontend/src/app/layouts/AppLayout.tsx
@@ -14,7 +14,9 @@ export function AppLayout() {
       />
 
       <SidebarInset className="min-h-0">
-        <Outlet />
+        <div className="flex min-h-0 flex-1 flex-col p-4 md:p-6">
+          <Outlet />
+        </div>
       </SidebarInset>
     </SidebarProvider>
   );
